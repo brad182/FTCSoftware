bbbe45e81c12ad15eae5846e40167548f54c923b
Brad <bradma123456@gmail.com>
2021-06-20 15:24:06 -0700

    folder

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class TeleOp {
+    public DcMotor frontRight = null;
+    public DcMotor frontLeft = null;
+}
